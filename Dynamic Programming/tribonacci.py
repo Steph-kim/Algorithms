ab9521429030e60d841693e1c9778fe9c1a46457
Steph-kim <z5206897@student.unsw.edu.au>
2022-05-02 20:44:52 +1000

    add algos

---
+++ Dynamic Programming/tribonacci.py
@@ -0,0 +1,23 @@
+def tribonacci(n):
+    # if n == 0: 
+    #     return 0
+    
+    # if n == 1 or n == 2:
+    #     return 1
+    
+    # return self.tribonacci(n-3) + self.tribonacci(n-2) + self.tribonacci(n-1)
+
+    if n < 2:
+        return n
+    
+    t0 = 0
+    t1 = t2 = 1 
+    tn = 0
+
+    for i in range(n-2):
+        tn = t0 + t1 + t2 
+        t0 = t1 
+        t1 = t2 
+        t2 = tn
+
+    return t2
