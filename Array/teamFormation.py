72878ffba1af5cd26d99362fa75621582cd573ad
Steph-kim <z5206897@student.unsw.edu.au>
2023-03-16 01:09:49 +1100

    fixed teamFormation question

--- Array/teamFormation.py
+++ Array/teamFormation.py
@@ -1,23 +1,34 @@
-def countTeams(ratings, queries):
-    count = {}
+def countTeams(rating, queries):
+
     res = []
-    for rating in ratings:
-        if rating in count:
-            count[rating] += 1
-        else:
-            count[rating] = 1
-            
     for l, r in queries:
-        subArray = ratings[l:r]
+        seen = {}
         teams = 0
-        for i in range(len(subArray)):
-            if count[subArray[i]] > 1:
-                teams += 1;
-                count[subArray[i]] -= 1
+        if r <= len(rating):
+            for i in range(l-1, r):
+                if rating[i] != 0:
+                    if rating[i] in seen and rating[i] != 0:
+                        teams += 1
+                        index = seen[rating[i]]
+                        seen.pop(rating[i])
+                        rating[index] = 0
+                        rating[i] = 0
+                    else: 
+                        seen[rating[i]] = i
         res.append(teams)
     return res
     
-ratings = [2,3,4,2]
-queries = [[1,4], [3,4]]
-print(countTeams(ratings, queries))
+rating=  [8, 8, 10, 1, 4, 5, 8, 10, 7, 6, 8, 3, 2, 5, 2, 3, 1, 8, 7, 7, 7, 3, 4, 2, 2, 3, 5, 10, 1, 10, 8, 7, 4, 5, 1, 4, 4, 5, 6, 2, 10, 7, 1, 7]
+queries= [[43, 44], [2, 36], [21, 26], [37, 43], [29, 43], [34, 36], [26, 27], [35, 42], [14, 34], [23, 44], [36, 41], [17, 37], [42, 45], [5, 35], [18, 34], [16, 17]]
+print(countTeams(rating, queries))
 
+
+"""
+ 0  s                            10                            20                              30             e              40        43
+[8, 0, 00, 0, 0, 0, 0, 00, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 00, 0, 00, 8, 7, 0, 0, 0, 0, 4, 5, 6, 2, 10, 7, 1, 7]
+teams = 16
+
+res = [0, 16, 0, 0]
+
+
+"""
